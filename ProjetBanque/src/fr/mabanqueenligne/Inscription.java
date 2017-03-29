dde7a54cb617c5bd5f923532488e3ffe59c58dfe
djamenyann <djamen.yann@yahoo.com>
2017-03-29 14:22:13 +0200

    Servlets Login et inscription

---
+++ ProjetBanque/src/fr/mabanqueenligne/Inscription.java
@@ -0,0 +1,56 @@
+package fr.mabanqueenligne;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Inscription
+ */
+@WebServlet("/Inscription")
+public class Inscription extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Inscription() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out= response.getWriter();
+		String nom,prenom,nationalité,sexe,adresse,codepostal,ville,email,password,statut;
+		String tel;
+		String naissance;
+		nom= request.getParameter("nom");
+		prenom= request.getParameter("prenom");
+		nationalité= request.getParameter("nationalité");
+		sexe= request.getParameter("sexe");
+		adresse= request.getParameter("adresse");
+		codepostal= request.getParameter("codepostal");
+		ville= request.getParameter("ville");
+		email= request.getParameter("email");
+		password= request.getParameter("password");
+		statut= request.getParameter("statut");
+		tel= request.getParameter("");
+		naissance= request.getParameter("naissance");
+
+		
+	}
+}
