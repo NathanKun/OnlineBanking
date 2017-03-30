5b2699787367aa7f9b006f9af0d34c4566900e3e
JunyangHE <nathanhejunyang@gmail.com>
2017-03-30 17:31:24 +0200

    Moved web pages to WebContent.

--- ProjetBanque/src/servlet/Inscription.java
+++ ProjetBanque/src/servlet/Inscription.java
@@ -43,7 +43,7 @@ public class Inscription extends HttpServlet {
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// On recupere les parametres entrés dans le formulaire HTML
+		// On recupere les parametres entrï¿½s dans le formulaire HTML
 		String nom= request.getParameter("nom");
 		String prenom= request.getParameter("prenom");
 		String nationalite= request.getParameter("nationalite");
@@ -57,23 +57,26 @@ public class Inscription extends HttpServlet {
 		String tel= request.getParameter("tel");
 		String naissance= request.getParameter("naissance");
 		PrintWriter out= response.getWriter();
-		Random rn = new Random();
+		/*Random rn = new Random();
 		String login= String.valueOf(rn.nextInt(99999999)) ;
 		String acc_number= login + "01" ;
 		
-		out.println("Espace créé");
+		out.println("Espace crï¿½ï¿½");
 		out.println("Votre login est "+ login);
 		Client c =new Client(0,login,password,prenom,nom,new DateTime(1993, 03, 03, 13, 45),
 				nationalite,sexe,adresse,codepostal,ville,tel,email,statut,null, 
 				new DateTime(2017, 03, 03, 13, 45));
-		//On ajoute les infos du client dans la base de données
+		//On ajoute les infos du client dans la base de donnï¿½es
 		DaoClient.addClient(c);
 	
 		c = DaoClient.findClientByLogin(login);
-		//On créé un compte courant pour le nouveau client
+		//On crï¿½ï¿½ un compte courant pour le nouveau client
 		Account acc=new Account(0,acc_number,c.getClt_id(),BigDecimal.ZERO,BigDecimal.ZERO,1);
 		DaoAccount.addAccount(acc);
 		out.println("Votre numero de compte est  "+ acc_number );
+*/
+		out.println("eparne = " + request.getParameter("epargneCheckBox"));
+		out.println("titre = " + request.getParameter("titreCheckBox"));
 		
 	}
 }
